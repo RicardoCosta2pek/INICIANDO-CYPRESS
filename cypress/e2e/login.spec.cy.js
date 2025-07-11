5ac9b99c43c9680a9a284a2475fa68a48808255e
Ricardo Beserra da Costa <ricardo.beserradacosta@gmail.com>
2025-07-11 10:46:46 -0300

    User update Cenario added

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,3 +1,4 @@
+import { select } from 'async'
 import userData from '../fixtures/users/userData.json'
 
 describe('template spec', () => {
@@ -8,16 +9,36 @@ describe('template spec', () => {
     loginButton: "[type='submit']",
     sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
     dashboardGrid: ".orangehrm-dashboard-grid",
-    wrongCredentialAlert: "[role='alert']"
+    wrongCredentialAlert: "[role='alert']",
+    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+    firstNameField: "[name='firstName']",
+    lastNameField: "[name='lastName']",
+    genericField: ".oxd-input--active",
+    dataField: "[placeholder='yyyy-dd-mm']",
+    dateCloseButton: ".--close",
+    submitButton: "[type='submit']"
+
   }
 
-  it('Login Sucessed!', () => {
+  it.only('User Info Update - Sucessed!', () => {
     cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
     cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get(selectorsList.dashboardGrid)
+    cy.get(selectorsList.myInfoButton).click()
+    cy.get(selectorsList.firstNameField).clear().type('FirstNameTest')
+    cy.get(selectorsList.lastNameField).clear().type('LastNameTest')
+    cy.get(selectorsList.genericField).eq(3).clear().type('123Test')
+    cy.get(selectorsList.genericField).eq(4).clear().type('1234Test')
+    cy.get(selectorsList.genericField).eq(5).clear().type('123456')
+    cy.get(selectorsList.genericField).eq(6).clear().type('2030-07-11')
+    cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.submitButton).eq(0).click()
+    cy.get('body').should('contain', 'Successfully Updated')
+    cy.get('.oxd-toast-close')
+
   })
     it('Login Fail!', () => {
     cy.visit('/auth/login')
